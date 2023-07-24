2d6e4abfc0f743ae5a316b97588203788f24d856
unknown <yuchenweng7@realtek.com>
2023-07-24 16:29:58 +0800

    test final

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'sample.html')));
+
 app.use('/tasks', tasksRouter);
 
 // catch 404 and forward to error handler
